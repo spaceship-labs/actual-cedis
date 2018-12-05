812290946315fedc6b23e03fa4ae4b6e92687ae6
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-12-05 16:33:47 -0500

    login functionality and auth service with token

---
+++ src/services/api.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { getToken } from 'services/auth';
+
+const API_BASE = 'http://127.0.0.1:1337';
+
+axios.defaults.baseURL = API_BASE;
+const get = (url, params = {}) => axios.get(url, { params });
+const put = (url, params = {}) => axios.put(url, params);
+const post = (url, params = {}) => axios.post(url, params);
+const destroy = (url, params = {}) => axios.delete(url, params);
+
+axios.interceptors.request.use(function(config) {
+  const token = getToken();
+  config.headers.Authorization = `JWR ${token}`;
+  return config;
+});
+
+function objectToForm(obj, form, level) {
+  let f = form || new FormData();
+
+  for (let k in obj) {
+    if (obj.hasOwnProperty(k)) {
+      let levelProp = level ? level + '[' + k + ']' : k;
+
+      if (
+        typeof obj[k] === 'object' &&
+        obj[k] != null &&
+        !(obj[k] instanceof File)
+      ) {
+        objectToForm(obj[k], f, levelProp);
+        continue;
+      }
+
+      f.set(levelProp, obj[k]);
+    }
+  }
+
+  return f;
+}
+
+export default {
+  API_BASE,
+  getToken,
+  login: {
+    do: params => {
+      const loginParams = {
+        ...params,
+        schema: 'user',
+        provider: 'local',
+      };
+      return post('/auth/signin', loginParams);
+    },
+  },
+};
