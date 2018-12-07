170ec0a85f4aa116d509d4723bc83a79f741c830
jose2802 <140300080@ucaribe.edu.mx>
2018-12-06 21:57:10 -0500

    pop ups que se renderean dependiendo de lo que el usuario eligio, si se cancela o no el producto

---
+++ src/components/requestList/RequestConfirmAprove.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Row, Button } from 'antd';
+import { RequestPop, RequestH3 } from './request.style';
+
+export default class extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      test: null,
+    };
+  }
+
+  render() {
+    const { toogle, handleClickAY, handleClickAN } = this.props;
+    const { test } = this.state;
+    if (toogle === true) {
+      return (
+        <RequestPop width="100px" height="auto">
+          <div className="request-bar-item flex column">
+            <Row type="flex">
+              <RequestH3
+                font="18px"
+                color="black"
+                weight="bold"
+                transform="uppercase"
+                width="100%"
+                align="center"
+              >
+                ¿Esta seguro que desea aprobar la siguiente operación?
+                {test}
+              </RequestH3>
+            </Row>
+            <Row type="flex" justify="center">
+              <div className="flex row center">
+                <Button type="danger" size="large" onClick={handleClickAN}>
+                  Cancelar
+                </Button>
+              </div>
+              <div className="flex row center">
+                <Button type="primary" size="large" onClick={handleClickAY}>
+                  Aceptar
+                </Button>
+              </div>
+            </Row>
+          </div>
+        </RequestPop>
+      );
+    }
+    return <div />;
+  }
+}
