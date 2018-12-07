8921c7fcb66f3ed91d84a4f6833d2abe728e6835
jose2802 <140300080@ucaribe.edu.mx>
2018-12-06 21:40:46 -0500

    Se agrega al options del SideBar el path para visualizar el container de requestList asi como en el AppRouter

--- src/containers/App/AppRouter.js
+++ src/containers/App/AppRouter.js
@@ -15,6 +15,10 @@ const routes = [
     path: 'authCheck',
     component: asyncComponent(() => import('../AuthCheck')),
   },
+  {
+    path: 'requestList',
+    component: asyncComponent(() => import('../RequestList/requestList')),
+  },
 ];
 
 class AppRouter extends Component {
