5fc211d29e5135a5cd85f217a053e61cc0a6f3e1
Pablo Melendez <pablofer.melendez@gmail.com>
2018-12-14 16:56:14 -0500

    babel configs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DashApp from './dashApp';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(<DashApp />, document.getElementById('root'));
@@ -9,4 +9,4 @@ ReactDOM.render(<DashApp />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
