55f921999607e26d06975b496620ad5d78af071e
jose2802 <140300080@ucaribe.edu.mx>
2018-12-18 18:50:54 -0500

    Se le agregan los estados faltantes, y los metodos junto con un obtejo de prueba para pasarselo a CancelContent

--- src/containers/CancelRequest/CancelRequest.js
+++ src/containers/CancelRequest/CancelRequest.js
@@ -18,6 +18,12 @@ import { propsSelector } from './selectors';
 class CancelRequest extends Component {
   constructor(props) {
     super(props);
+    this.handleClickAproveAll = this.handleClickAproveAll.bind(this);
+    this.handleClickDeniedAll = this.handleClickDeniedAll.bind(this);
+    this.handleClickCAllN = this.handleClickCAllN.bind(this);
+    this.handleClickCAllY = this.handleClickCAllY.bind(this);
+    this.handleClickDAllN = this.handleClickDAllN.bind(this);
+    this.handleClickDAllY = this.handleClickDAllY.bind(this);
     this.handleClickAprove = this.handleClickAprove.bind(this);
     this.handleClickDenied = this.handleClickDenied.bind(this);
     this.handleClickAN = this.handleClickAN.bind(this);
@@ -26,12 +32,14 @@ class CancelRequest extends Component {
     this.handleClickDY = this.handleClickDY.bind(this);
     this.goBack = this.goBack.bind(this);
     this.state = {
-      toogleBack: false,
-      toogleOption: true,
+      toogleBack: true,
+      toogleOption: false,
       toogleConfirmA: false,
       toogleConfirmD: false,
-      autorizado: null,
-      rechazado: null,
+      toogleCAll: false,
+      toogleDAll: false,
+      autorizado: true,
+      rechazado: false,
     };
   }
 
@@ -42,6 +50,40 @@ class CancelRequest extends Component {
       },
     } = this.props;
     getCancelRequest(id);
+  }
+
+  handleClickAproveAll() {
+    this.setState({ toogleCAll: true, toogleOption: false, toogleBack: false });
+  }
+
+  handleClickCAllN() {
+    this.setState({ toogleCAll: false, toogleOption: false, toogleBack: true });
+  }
+
+  handleClickCAllY() {
+    this.setState({
+      toogleCAll: false,
+      autorizado: true,
+      toogleBack: true,
+      rechazado: false,
+    });
+  }
+
+  handleClickDeniedAll() {
+    this.setState({ toogleDAll: true, toogleOption: false });
+  }
+
+  handleClickDAllN() {
+    this.setState({ toogleDAll: false, toogleOption: false, toogleBack: true });
+  }
+
+  handleClickDAllY() {
+    this.setState({
+      toogleDAll: false,
+      rechazado: true,
+      toogleBack: true,
+      autorizado: false,
+    });
   }
 
   handleClickAprove() {
@@ -88,15 +130,64 @@ class CancelRequest extends Component {
   }
 
   render() {
-    const objeto = {
-      description: { id: 1, value: 'Mueble Blanco' },
-      code: 'lyoco',
-      color: 'red',
-      quantity: 2,
-      delivery: '11/12/2018',
-      price: '$1,500.00 MXN',
-      visible: true,
-    };
+    // const objeto1 = {
+    //   description: { id: 1, value: 'Mueble Negro' },
+    //   code: '12345',
+    //   color: 'black',
+    //   quantity: 3,
+    //   delivery: '11/12/2018',
+    //   price: '$1,500.00 MXN',
+    //   visible: true,
+    // };
+
+    // const objeto2 = {
+    //   description: { id: 2, value: 'Mueble Blanco' },
+    //   code: '67890',
+    //   color: 'white',
+    //   quantity: 2,
+    //   delivery: '18/12/2018',
+    //   price: '$2,500.00 MXN',
+    //   visible: true,
+    // };
+
+    const data = [
+      {
+        description: { id: 1, value: 'Mueble Blanco' },
+        code: '67890',
+        color: 'white',
+        quantity: 2,
+        delivery: '11/12/2018',
+        price: '$1,500.00 MXN',
+        visible: true,
+      },
+      {
+        description: { id: 2, value: 'Mueble Negro' },
+        code: '12345',
+        color: 'black',
+        quantity: 3,
+        delivery: '18/12/2018',
+        price: '$2,500.00 MXN',
+        visible: true,
+      },
+      {
+        description: { id: 3, value: 'Mueble Azul' },
+        code: '13579',
+        color: 'blue',
+        quantity: 5,
+        delivery: '21/12/2018',
+        price: '$3,500.00 MXN',
+        visible: true,
+      },
+      {
+        description: { id: 4, value: 'Mueble Dorado' },
+        code: '7587969',
+        color: 'dorado',
+        quantity: 2,
+        delivery: '25/12/2018',
+        price: '$5,500.00 MXN',
+        visible: true,
+      },
+    ];
 
     const {
       toogleBack,
@@ -105,13 +196,16 @@ class CancelRequest extends Component {
       toogleConfirmD,
       autorizado,
       rechazado,
+      toogleCAll,
+      toogleDAll,
     } = this.state;
 
     return (
       <LayoutContentWrapper style={{ height: 'auto' }}>
         <LayoutContent>
           <CancelContent
-            object={objeto}
+            data={data}
+            // todes={data}
             toogleOption={toogleOption}
             handleClickAprove={this.handleClickAprove}
             handleClickDenied={this.handleClickDenied}
@@ -125,6 +219,14 @@ class CancelRequest extends Component {
             toogleConfirmD={toogleConfirmD}
             handleClickDN={this.handleClickDN}
             handleClickDY={this.handleClickDY}
+            handleClickAproveAll={this.handleClickAproveAll}
+            handleClickDeniedAll={this.handleClickDeniedAll}
+            toogleCAll={toogleCAll}
+            toogleDAll={toogleDAll}
+            handleClickCAllY={this.handleClickCAllY}
+            handleClickCAllN={this.handleClickCAllN}
+            handleClickDAllY={this.handleClickDAllY}
+            handleClickDAllN={this.handleClickDAllN}
           />
         </LayoutContent>
       </LayoutContentWrapper>
