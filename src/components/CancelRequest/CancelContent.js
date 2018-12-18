173ba4c1922fa033e66f9c9b671995304aa7c8df
jose2802 <140300080@ucaribe.edu.mx>
2018-12-18 18:48:43 -0500

    Se agregan los pop ups al nivel de CancelContent, y al TestMain se le agrega un objeto de prueba para renderearlo varias veces y ver como se comporta el componente y ver si esta bien la maquetaciÃn

--- src/components/CancelRequest/CancelContent.js
+++ src/components/CancelRequest/CancelContent.js
@@ -1,12 +1,16 @@
 import React from 'react';
-// import RequestContent from './RequestContent';
-import TestMain from './TestMain';
-import SolicitudBar from './SolicitudBar';
-import OrderText from './OrderText';
-import CambiosBar from './CambiosBar';
+import TestMain from './TestMain/TestMain';
+import SolicitudBar from './SolicitudBar/SolicitudBar';
+import OrderText from './OrderText/OrderText';
+import CambiosBar from './CambiosBar/CambiosBar';
+import TestRequestConfirmAprove from './TestMain/TestRequestConfirm/TestRequestConfirmAprove';
+import TestRequestConfirmDenied from './TestMain/TestRequestConfirm/TestRequestConfirmDenied';
+import TestRequestAproveAll from './TestMain/TestRequestConfirm/TestRequestAproveAll';
+import TestRequestCancelAll from './TestMain/TestRequestConfirm/TestRequestCancelAll';
 
 const CancelContent = ({
-  object,
+  data,
+  // todes,
   toogleOption,
   handleClickAprove,
   handleClickDenied,
@@ -20,12 +24,49 @@ const CancelContent = ({
   toogleConfirmD,
   handleClickDN,
   handleClickDY,
+  handleClickAproveAll,
+  handleClickDeniedAll,
+  toogleCAll,
+  toogleDAll,
+  handleClickCAllN,
+  handleClickCAllY,
+  handleClickDAllN,
+  handleClickDAllY,
 }) => (
   <div>
-    <SolicitudBar />
+    <SolicitudBar
+      handleClickAproveAll={handleClickAproveAll}
+      handleClickDeniedAll={handleClickDeniedAll}
+    />
     <OrderText />
-    <TestMain
-      object={object}
+    <div>
+      {data.map(d => (
+        <TestMain
+          object={d}
+          toogleOption={toogleOption}
+          handleClickAprove={handleClickAprove}
+          handleClickDenied={handleClickDenied}
+          toogleBack={toogleBack}
+          autorizado={autorizado}
+          rechazado={rechazado}
+          goBack={goBack}
+          toogleConfirmA={toogleConfirmA}
+          handleClickAN={handleClickAN}
+          handleClickAY={handleClickAY}
+          toogleConfirmD={toogleConfirmD}
+          handleClickDN={handleClickDN}
+          handleClickDY={handleClickDY}
+          toogleCAll={toogleCAll}
+          toogleDAll={toogleDAll}
+          handleClickCAllN={handleClickCAllN}
+          handleClickCAllY={handleClickCAllY}
+          handleClickDAllY={handleClickDAllY}
+          handleClickDAllN={handleClickDAllN}
+        />
+      ))}
+    </div>
+    {/* <TestMain
+      object={todes}
       toogleOption={toogleOption}
       handleClickAprove={handleClickAprove}
       handleClickDenied={handleClickDenied}
@@ -39,8 +80,34 @@ const CancelContent = ({
       toogleConfirmD={toogleConfirmD}
       handleClickDN={handleClickDN}
       handleClickDY={handleClickDY}
-    />
+      toogleCAll={toogleCAll}
+      toogleDAll={toogleDAll}
+      handleClickCAllN={handleClickCAllN}
+      handleClickCAllY={handleClickCAllY}
+      handleClickDAllY={handleClickDAllY}
+      handleClickDAllN={handleClickDAllN}
+    /> */}
     <CambiosBar />
+    <TestRequestConfirmAprove
+      toogle={toogleConfirmA}
+      handleClickAN={handleClickAN}
+      handleClickAY={handleClickAY}
+    />
+    <TestRequestConfirmDenied
+      toogle={toogleConfirmD}
+      handleClickDN={handleClickDN}
+      handleClickDY={handleClickDY}
+    />
+    <TestRequestAproveAll
+      toogle={toogleCAll}
+      handleClickCAllN={handleClickCAllN}
+      handleClickCAllY={handleClickCAllY}
+    />
+    <TestRequestCancelAll
+      toogle={toogleDAll}
+      handleClickDAllY={handleClickDAllY}
+      handleClickDAllN={handleClickDAllN}
+    />
   </div>
 );
 
