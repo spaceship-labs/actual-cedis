0b586a9a87025c1e8f370e48249b332ab0b9743e
jose2802 <140300080@ucaribe.edu.mx>
2019-01-28 14:51:31 -0500

    se modifica el selector del contaier para exportar el loading, cancel y products selector

--- src/containers/CancelRequest/selectors.js
+++ src/containers/CancelRequest/selectors.js
@@ -1,12 +1,14 @@
-const cancelRequestSelector = ({ objects }) => objects.cancelRequest;
-
-const containerSelector = state => {
-  const {
-    objects: { cancelRequest },
-    lists: { products },
-  } = state;
-
-  return { cancelRequest, products };
+export const cancelSelector = ({ objects }) => objects.cancelRequest;
+export const loadingSelector = ({ objects }) => objects.loadingCancel;
+export const productSelector = ({ lists }) => lists.products;
+export const containerSelector = ({ objects, lists }) => {
+  const { cancelRequest, loadingCancel } = objects;
+  const { products } = lists;
+  return { cancelRequest, products, loadingCancel };
 };
-
-export default { cancelRequestSelector, containerSelector };
+export default {
+  cancelSelector,
+  loadingSelector,
+  productSelector,
+  containerSelector,
+};
