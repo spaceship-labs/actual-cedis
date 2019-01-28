4c3a9a373c063595275368e5f1de1d8fc3b65dbe
jose2802 <140300080@ucaribe.edu.mx>
2019-01-28 14:40:34 -0500

    se modifica el redurcer del redux/objects para agregar el getCancel

--- src/redux/objects/reducer.js
+++ src/redux/objects/reducer.js
@@ -7,8 +7,10 @@ const initialState = {
 
 function objectsReducer(state = initialState, { type, payload }) {
   switch (type) {
+    case actions.getCancel.type:
+      return { ...state, loadingCancel: true };
     case actions.setCancel.type:
-      return { ...state, cancelRequest: payload };
+      return { ...state, cancelRequest: payload, loadingCancel: false };
     case actions.setOrder.type:
       return { ...state, order: payload, loading: false };
     default:
