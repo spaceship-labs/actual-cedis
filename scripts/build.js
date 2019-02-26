cce699b1eb5629abe6bfea34c4e90383aca5f740
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-02-26 01:54:35 -0500

    path

--- scripts/build.js
+++ scripts/build.js
@@ -1,3 +1,4 @@
+const path = require('path');
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
@@ -12,7 +13,7 @@ process.on('unhandledRejection', err => {
 // Ensure environment variables are read.
 require('../config/env');
 
-const path = require('path');
+// const path = require('path');
 const chalk = require('chalk');
 const fs = require('fs-extra');
 const webpack = require('webpack');
